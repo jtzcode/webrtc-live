de0b5f384a0b2909775a0f66e9f14a2b50f2d74d
Tianze Jiang （江天择） <tianze.jiang@citrix.com>
2021-09-03 14:57:19 +0800

    List devices

--- scripts/client.js
+++ scripts/client.js
@@ -1,14 +1,24 @@
 'use strict'
 
 const mediaStreamConstraints = {
-    video: true,
-    audio: true
+    video: true
+    //audio: true
 };
 
 const localVideo = document.querySelector("video");
+const deviceList = document.querySelector(".device-list");
 
 navigator.mediaDevices.getUserMedia(mediaStreamConstraints).then((mediaStream) => {
     localVideo.srcObject = mediaStream;
 }).catch((error) => {
     console.error("navigator.getUserMedia error: ", error);
+});
+
+navigator.mediaDevices.enumerateDevices().then((devices) => {
+    devices.forEach(device => {
+        console.log("Device id: " + device.deviceId + " Kind: " + device.kind + " Label: " + device.label);
+        const item = document.createElement('li');
+        item.innerHTML = "Device id: " + device.deviceId + " Kind: " + device.kind + " Label: " + device.label;
+        deviceList.appendChild(item);
+    });
 });
